226ea85f7b9986bdbef132b873eda0fd9749c9ab
Shah-jahan-Mukhtar <shahjahanmukhtar62@gmail.com>
2022-01-08 22:49:12 +0500

    Navigation route

--- App.js
+++ App.js
@@ -15,7 +15,10 @@ const  App=()=> {
     <NavigationContainer >
       <Stack.Navigator >
         <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name="ColorPalette" component={ColorPalette}/>
+        <Stack.Screen name="ColorPalette" component={ColorPalette}
+        options={({ route }) => ({ title: route.params.paletteName })}
+  
+        />
 
       </Stack.Navigator>
     </NavigationContainer>
