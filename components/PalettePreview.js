5b50ffc4155a96d7c1a92991acfb894976719411
Shah-jahan-Mukhtar <shahjahanmukhtar62@gmail.com>
2022-01-11 15:33:00 +0500

    PalettePreview

--- components/PalettePreview.js
+++ components/PalettePreview.js
@@ -1,15 +1,49 @@
-import React from 'react'
-import  {View,Text,TouchableOpacity} from 'react-native';
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+} from "react-native";
 
+const PalettePreview = ({ handlePress, colorPalette }) => {
+  return (
+    <TouchableOpacity onPress={handlePress}>
+      <Text style={styles.text}>{colorPalette.paletteName}</Text>
+      <FlatList
+        // horizontal={true}
+        style={styles.list}
+        data={colorPalette.colors.slice(0, 5)}
+        keyExtractor={(item) => item.colorName}
+        renderItem={({ item }) => (
+          <View style={[{ backgroundColor: item.hexCode }, styles.box]} />
+        )}
+      />
+    </TouchableOpacity>
+  );
+};
 
-const PalettePreview=(handlePress,colorPalette)=>{
-    return(
-        <TouchableOpacity onPress={handlePress}> 
-        <Text >{colorPalette.paletteName}</Text>
-        </TouchableOpacity>
-    )
-}
-
-
+const styles = StyleSheet.create({
+  text: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  box: {
+    height: 30,
+    width: 30,
+    marginRight: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 10,
+  },
+  list: {
+    marginBottom: 20,
+    flexDirection: "row",
+  },
+});
 
 export default PalettePreview;
